cedaeddcc527b8eb7d17dea43a9d1e21a009c2ea
sofiabase1995 <112864743+sofiabase1995@users.noreply.github.com>
2023-03-13 15:17:45 +0300

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: "BASE Henkilöstöpalvelut - Laadukkaita palveluita tekijöille",
   description: "BASE Henkilöstöpalvelut - Laadukkaita palveluita tekijöille",
-  url: process.env.ELEVENTY_ENV === "production" ? "https://sofiabase1995.github.io/base.fi" : "",
+  url: process.env.ELEVENTY_ENV === "production" ? "https://basefi.github.io/base.fi" : "",
   buildTime: new Date(),
 };
